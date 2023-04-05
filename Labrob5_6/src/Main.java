0b156c9692c563eaba3211f0221ecd44ba732d53
Makss232 <129625678+Makss232@users.noreply.github.com>
2023-04-05 16:01:50 +0200

    Add files via upload

---
+++ Labrob5_6/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        //        TASK1
+        int a=31;
+        int b=4;
+        float c1;
+        c1=(float) a/b;
+
+        System.out.println(c1);
+//        TASK2
+        int c=15;
+        int d= c/10;
+        int e= c%10;
+        int sum=d+e;
+        System.out.println(c);
+        System.out.println("Сума двохзначного числа:" + sum);
+//        TASK3
+        double p=27.4;
+        int q =(int)(p+0.5);
+        System.out.println(q);;
+    }
+}
